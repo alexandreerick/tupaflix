3c8a8251793fa732dd98867a3b664995412ae525
alexandreerick <erickalexandrex@gmail.com>
2020-06-21 10:50:30 -0300

    adicionando variavel de ambiente

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,12 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes";
 import path from "path";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+const { PORT } = process.env;
 
 app.use(cors());
 app.use(express.json());
@@ -11,4 +15,4 @@ app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
-app.listen(3333);
+app.listen(PORT);
